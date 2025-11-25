711a8c2f3baf90c387ee2c3b73ca60b37a4d271e
enzoqta <149586663+enzoqta@users.noreply.github.com>
2025-11-24 23:10:35 -0300

    Add files via upload

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -1,19 +1,21 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Home from './Home';
 import Atv01 from './Atv01';
 import Atv02 from './Atv02';
 import Atv03 from './Atv03';
+import Atv04 from './Atv04';
 
 export default function MinhasRotas() {
     return (
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Atv01" element={<Atv01 />} />
-            <Route path="/Atv02" element={<Atv02 />} />
-            <Route path="/Atv03" element={<Atv03 />} />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Atv01" element={<Atv01 />} />
+                <Route path="/Atv02" element={<Atv02 />} />
+                <Route path="/Atv03" element={<Atv03 />} />
+                <Route path="/Atv04" element={<Atv04 />} />
+            </Routes>
         </BrowserRouter>
     );
 }
